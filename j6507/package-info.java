538b0696ba59e5e0877350c4669ee9afde7c6d00
Jaeman An <ajmbell@bawi.org>
2011-11-29 04:44:36 +0900

    필요없는 파일 모두 제거

--- j6507/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- Cotains the 6507 class, the CPU for the 2600.  
-  This is separate because the the 6507 class (actually a 6502 emulator) is 
-  designed independently of the system that is using it.  
-  The IfcSystem class is an interface that allows the 6507 to interact with
-  this system.
-
- */
-package jstella.j6507;
